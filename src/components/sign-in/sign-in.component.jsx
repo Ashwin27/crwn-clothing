fd2ee9f74091cb4619cd5325ea253992d536905f
Ashwin Raman <ashwin.raman@instacart.com>
2021-05-09 16:42:57 -0400

    Adding a Signup page + custom button

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import FormInput from '../form-input/form-input.component'
+import './sign-in.styles.scss'
+
+import CustomButton from '../custom-button/custom-button.component'
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({ email: "", password: ""})
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value })
+  }
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h1>I already have an account</h1>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput name="email" type="email" handleChange={this.handleChange} value={this.state.email} label="email" required/>
+          <FormInput name="password" type="password" handleChange={this.handleChange} value={this.state.password} label="password" required/>
+
+          <CustomButton type="submit" value="Submit Form" onClick={this.handleSubmit} children="Sign In"/>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import SignIn from '../../components/sign-in/sign-in.component'
+
+const SignInAndSignUpPage = () => (
+  <SignIn />
+);
+
+export default SignInAndSignUpPage;
