1021f2e0e2a6199f4db05c30d899701cf0066e32
Ashwin Raman <ashwin.raman@instacart.com>
2021-05-09 14:32:38 -0400

    Adding a skeleton shop page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Switch, Route} from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
 const HatsPage = () => (
   <div>
@@ -15,7 +16,8 @@ function App() {
   return (
     <Switch>
       <Route exact path="/" component={HomePage}/>
-      <Route path="/shop/hats" component={HatsPage}/>
+      <Route exact path="/shop" component={ShopPage}/>
+      <Route exact path="/shop/hats" component={HatsPage}/>
     </Switch>
   );
 }
