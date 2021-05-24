0b73a4184eed97f2451c3f78efd957e949100edf
Ashwin Raman <ashwin.raman@instacart.com>
2021-05-24 17:16:25 -0400

    Completed Checkout Page with Add/Remove Items

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux';
-import { clearItemFromCart } from '../../redux/cart/cart.actions';
+import { addItem, clearItemFromCart, removeItem } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss'
 
-const CheckoutItem = ( { cartItem, dispatch } ) => {
+const CheckoutItem = ( { cartItem, clearItem, addItem, removeItem } ) => {
   const {name, quantity, price, imageUrl} = cartItem;
   console.log(cartItem);
   return (
@@ -13,11 +13,21 @@ const CheckoutItem = ( { cartItem, dispatch } ) => {
         <img alt='item' src={imageUrl}/>
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
+      </span>
       <span className="price">{price}</span>
-      <div className="remove-button" onClick={() => dispatch(clearItemFromCart(cartItem))}>&#10005;</div>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>&#10005;</div>
     </div>
   );
 };
 
-export default connect(null)(CheckoutItem);
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
