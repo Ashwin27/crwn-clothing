f5fa528465f092acc5947e2b075c1d36477cce6e
Ashwin Raman <ashwin.raman@instacart.com>
2021-06-03 20:16:05 -0400

    Fixing up the app

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -5,10 +5,10 @@ const StripeCheckoutButton = ({ price }) => {
   const priceInCents = price * 100;
   const publishableKey = "pk_test_51Iw8A4Eo7uvfYAEnf79mGgRHR4NPVdlDCI5h9Rsk1XSml0C8iLxAL05ajYLzUI7Y8vbSNI1pSsAnsDPhUMtz4kmf00nS0hCAJz";
 
-  onToken = token => {
+  const onToken = token => {
     console.log(token);
     alert('Payment successful');
-  }
+  };
 
   return (
     <StripeCheckout 
@@ -16,13 +16,12 @@ const StripeCheckoutButton = ({ price }) => {
       name='CRWN CLOTHING Ltd.'
       billingAddress
       shippingAddress
-      image=''
+      image='https://svgshare.com/i/CUz.svg'
       panelLabel='Pay Now'
       amount={priceInCents}
-      description={`Your price is ${price}`}
-      token={this.onToken}
+      description={`Your price is $${price}`}
+      token={onToken}
       stripeKey={publishableKey}
-      svgUrl='https://sendeyo.com/up/d/f3eb2117da'
     />
   );
 }
